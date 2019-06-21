34eaf08688dc7538650163ba66e6f0413c1aef3a
Isuru Warnakulasooriya <isuru.warnakulasooriya@sap.com>
2019-06-21 10:41:32 -0700

    function to provide formatted duration

--- src/main/java/org/warn/utils/datetime/DateTimeUtil.java
+++ src/main/java/org/warn/utils/datetime/DateTimeUtil.java
@@ -1,6 +1,7 @@
 package org.warn.utils.datetime;
 
 import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
 
 public class DateTimeUtil {
 
@@ -9,4 +10,32 @@ public class DateTimeUtil {
 	
 	public static final SimpleDateFormat fullTimestampSDF = new SimpleDateFormat( FULL_TS_FORMAT );
 	public static final SimpleDateFormat dateSDF = new SimpleDateFormat( DATE_ONLY_TS_FORMAT );
+	
+	public static String formatDuration( TimeUnit timeUnit, long duration ) {
+		
+		long seconds;
+		long minutes;
+		long hours;
+		
+		if( timeUnit == TimeUnit.MILLISECONDS ) {
+			seconds = TimeUnit.MILLISECONDS.toSeconds( duration ) % 60;
+			minutes = TimeUnit.MILLISECONDS.toMinutes( duration ) % 60;
+			hours = TimeUnit.MILLISECONDS.toHours( duration );
+		
+		} else if( timeUnit == TimeUnit.SECONDS ) {
+			seconds = TimeUnit.SECONDS.toSeconds( duration ) % 60;
+			minutes = TimeUnit.SECONDS.toMinutes( duration ) % 60;
+			hours = TimeUnit.SECONDS.toHours( duration );
+			
+		} else if( timeUnit == TimeUnit.HOURS ) {
+			seconds = TimeUnit.HOURS.toSeconds( duration ) % 60;
+			minutes = TimeUnit.HOURS.toMinutes( duration ) % 60;
+			hours = TimeUnit.HOURS.toHours( duration );
+			
+		} else {
+			throw new UnsupportedOperationException("TimeUnit " + timeUnit + " is not supported");
+		}
+		
+		return String.format("%02d:%02d:%02d", hours, minutes, seconds );
+	}
 }
